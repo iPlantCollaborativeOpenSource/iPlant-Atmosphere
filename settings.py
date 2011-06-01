291571ca34c4fbf95147d983f0e6e44da2ef09f4
seungjin <seungjin@seungjin.net>
2011-06-01 10:14:13 -0700

    split setting for git / security

--- settings.py
+++ settings.py
@@ -7,25 +7,61 @@
 # GitHub: seungjin
 #
 
-
-
 # Django settings for atmosphere project.
+
+
+from ConfigParser import RawConfigParser
+
+config = RawConfigParser()
+config.read('/home/atmosphere_dev/credentials_never_git_it.ini')
+
+DATABASE_USER = config.get('database', 'DATABASE_USER')
+DATABASE_PASSWORD = config.get('database', 'DATABASE_PASSWORD')
+DATABASE_HOST = config.get('database', 'DATABASE_HOST')
+DATABASE_PORT = config.get('database', 'DATABASE_PORT')
+DATABASE_ENGINE = config.get('database', 'DATABASE_ENGINE')
+DATABASE_NAME = config.get('database', 'DATABASE_NAME')
+#TEST_DATABASE_NAME = config.get('database', 'TESTSUITE_DATABASE_NAME')
+
+#SECRET_KEY = config.get('secrets','SECRET_KEY')
+#CSRF_MIDDLEWARE_SECRET = config.get('secrets', 'CSRF_MIDDLEWARE_SECRET')
+
+#SESSION_COOKIE_DOMAIN = config.get('cookies','SESSION_COOKIE_DOMAIN')
+
+#DEBUG = config.getboolean('debug','DEBUG')
+#TEMPLATE_DEBUG = config.getboolean('debug','TEMPLATE_DEBUG')
+#VIEW_TEST = config.getboolean('debug', 'VIEW_TEST')
+#INTERNAL_IPS = tuple(config.get('debug', 'INTERNAL_IPS').split())
+#if config.getboolean('debug', 'SKIP_CSRF_MIDDLEWARE'):
+#    MIDDLEWARE_CLASSES = tuple([x for x in list(MIDDLEWARE_CLASSES)
+#                                  if not x.endswith('CsrfMiddleware')])
+
+SERVER_EMAIL = config.get('email', 'SERVER_EMAIL')
+EMAIL_HOST = config.get('email', 'EMAIL_HOST')
+ADMINS = tuple(config.items('error mail'))
+MANAGERS = tuple(config.items('404 mail'))
+
+
+
+
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Seung-jin Kim', 'seungjin@email.arizona.edu'),
+    ('Seung-jin Kim', 'seungjin@email.arizona.edu'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+#DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+#DATABASE_USER = ''             # Not used with sqlite3.
+#DATABASE_PASSWORD = ''         # Not used with sqlite3.
+#DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+#DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
