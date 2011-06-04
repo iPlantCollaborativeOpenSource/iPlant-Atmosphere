5b471a7959e8b12add77ce60c57bcfb8a3d3dc6a
Seung-jin Kim <seungjin@seungjin.net>
2011-06-04 09:48:17 -0700

    get_user_instance_list task

---
+++ cloudservice/task/get_user_instance_list.py
@@ -0,0 +1,28 @@
+#
+# The contents of this file are subject to the terms listed in the LICENSE file you received with this code.
+#
+# Project: Atmosphere, iPlant Collaborative
+# Author: Seung-jin Kim
+# Twitter: @seungjin
+# GitHub: seungjin
+#
+
+
+from datetime import datetime
+from datetime import timedelta
+
+
+import boto
+from boto.ec2.connection import EC2Connection
+from boto.ec2.regioninfo import RegionInfo
+
+
+from atmosphere.cloudservice.models import *
+from urlparse import urlparse
+from django.utils import simplejson
+import atmosphere.cloudservice.api.v1.util as atmo_util
+
+import inspect
+
+def get_user_instance_list(userid):
+  pass
