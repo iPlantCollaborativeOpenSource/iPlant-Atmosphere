795a893dfa95f60f13c55b27b930d59212d1fd7d
Seung-jin Kim <seungjin@seungjin.net>
2011-06-04 06:29:23 -0700

    null

--- __init__.py
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2010, iPlant Collaborative, University of Arizona, Cold Spring Harbor Laboratories, University of Texas at Austin
-# This software is licensed under the CC-GNU GPL version 2.0 or later.
-# License: http://creativecommons.org/licenses/GPL/2.0/
-#
-# Author: Seung-jin Kim
-# Contact: seungjin@email.arizona.edu
-# Twitter: @seungjin
-#
