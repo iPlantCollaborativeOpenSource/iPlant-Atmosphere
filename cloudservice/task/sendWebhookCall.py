666643a47c6aa95de3537c308b3fbd0f2f85222a
Seung-jin Kim <seungjin@seungjin.net>
2011-06-05 11:55:17 -0700

    Celery que based sendWebhookCall function. TODO

---
+++ cloudservice/task/sendWebhookCall.py
@@ -0,0 +1,19 @@
+#
+# The contents of this file are subject to the terms listed in the LICENSE file you received with this code.
+#
+# Project: Atmosphere, iPlant Collaborative
+# Author: Seung-jin Kim
+# Twitter: @seungjin
+# GitHub: seungjin
+#
+
+
+
+
+
+# this should be celery task-able function. 
+def sendWebhookCall(destination,message):
+  pass
+
+
+
