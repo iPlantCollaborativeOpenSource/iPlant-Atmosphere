5ba2cb06d9b8daae720abd18115a06543591b01e
Seung-jin Kim <seungjin@seungjin.net>
2011-06-07 10:12:09 -0700

    web notification's run_cmd problem fix.

--- cloudservice/api/v1/instanceservice.py
+++ cloudservice/api/v1/instanceservice.py
@@ -142,7 +142,7 @@ def call(request) :
         
 
         notice_msg = "instance %s was launched with ip %s" % ( simplejson.loads(message)['instance-id'] , simplejson.loads(message)['public-ipv4'] )
-        run_cmd = lambda c : subprocess.Popen(c.split(), stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=False).stdout.read()
+        run_cmd = lambda c : subprocess.Popen(c.split(None,2), stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=False).stdout.read()
         cmd = '%s %s %s "%s"' % (node_path, sayjs_path, channel, notice_msg)
         run_cmd(cmd)
         #r, w, e = popen2.popen3('%s %s %s "%s"' % (node_path, sayjs_path, channel, notice_msg))
