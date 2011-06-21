9f80716f1a1949ee94d4ecb38a69ac17a2e01323
Seung-jin Kim <seungjin@seungjin.net>
2011-06-21 15:39:31 -0700

    eucalyptus's cloudservice work - in progress

--- cloudservice/eucalyptus/authorize_group.py
+++ cloudservice/eucalyptus/authorize_group.py
@@ -9,7 +9,7 @@
 
 
 
-from datetime import datetime
+from urlparse import urlparse
 
 import boto
 from boto.ec2.connection import EC2Connection
@@ -28,7 +28,6 @@ def open_port(user,port):
   else :
     user_list = user.split(",")
 
-
 def open_ping(user):
   if user == "*":
     users = Ec2_keys.objects.filter()
@@ -39,7 +38,6 @@ def open_ping(user):
     user_list = user.split(",")
   
   for user in user_list:
-    
     #http://boto.s3.amazonaws.com/ref/ec2.html#module-boto.ec2.securitygroup
     #class boto.ec2.securitygroup.SecurityGroup(connection=None, owner_id=None, name=None, description=None)
     #authorize(ip_protocol=None, from_port=None, to_port=None, cidr_ip=None, src_group=None)
@@ -53,17 +51,16 @@ def open_ping(user):
     #   Return type:  bool
     #   Returns:  True if successful.
 
-
-    ec2_key = Ec2_key.objects.get(username = user).ec2_access_key
-    ec2_secret = Ec2_key.objects.get(username = user).ec2_secret_key
-    ec2_url = Ec2_key.objects.get(username = user).ec2_url
+    ec2_key = Ec2_keys.objects.get(username = user).ec2_access_key
+    ec2_secret = Ec2_keys.objects.get(username = user).ec2_secret_key
+    ec2_url = Ec2_keys.objects.get(username = user).ec2_url
     region = RegionInfo(name="eucalyptus", endpoint=urlparse(ec2_url).netloc.split(":")[0])
     connection = boto.connect_ec2(aws_access_key_id=str(ec2_key),
                   aws_secret_access_key=str(ec2_secret),
-                                is_secure=False,
-                                region=region,
-                                port=8773,
-                                path="/services/Eucalyptus")
+                  is_secure=False,
+                  region=region,
+                  port=8773,
+                  path="/services/Eucalyptus")
     #volumes = connection.get_all_volumes(owner="edwintest")
     #sg = SecurityGroup(connection,owner_id=)
     
