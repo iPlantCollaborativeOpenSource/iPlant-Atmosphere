80b2cc9919c487e67aebf637bde33697fe57ba00
Seung-jin Kim <seungjin@seungjin.net>
2011-07-28 10:15:45 -0700

    flavers.py removed. I don't need it anymore. flavor is a concept from openstack. see openstack gate implementation

--- cloudservice/flavors.py
+++
@@ -1,8 +0,0 @@
-#
-# The contents of this file are subject to the terms listed in the LICENSE file you received with this code.
-#
-# Project: Atmosphere, iPlant Collaborative
-# Author: Seung-jin Kim
-# Twitter: @seungjin
-# GitHub: seungjin
-#
